3b05a3c5db51d3847341e2ad59f3d552a1ef1da9
EC2 Default User <ec2-user@ip-172-31-14-174.ap-southeast-1.compute.internal>
2018-10-24 15:43:32 +0000

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, :only => [:show, :index, :edit, :update]
+    
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def index
+    @users = User.all
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    if @user.id == current_user.id
+    else
+      redirect_to "/"
+      flash[:alert] = "You don't have permission to access"
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.id == current_user.id
+      @user.update(params.require(:user).permit(:name, :user_type, :banmas_id, :profile, :genre, :email, :password, :icon, :area, :gender, :waiting, :part))
+      redirect_to "/users/#{@user.id}/edit"
+      flash[:notice] = "Saved!"
+    else
+      redirect_to "/"
+      flash[:alert] = "You have no permission to access"
+    end
+  end
+  
+end
