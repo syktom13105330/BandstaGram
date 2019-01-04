d87188c342e55c48a815f01aec63b3c7ab3b9dc3
EC2 Default User <ec2-user@ip-172-31-14-25.ap-southeast-1.compute.internal>
2019-01-04 06:03:41 +0000

    config:set

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,15 +4,15 @@ require 'carrierwave/storage/fog'
 
  
 CarrierWave.configure do |config|
-   if Rails.env.development?
+   if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'bandstagram5'
+    config.fog_directory  = ENV['S3_BUCKET']
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AKIAIKLBBEAQOBAFVDRQ',
-      aws_secret_access_key: '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',
-      region: 'ap-northeast-1',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['S3_REGION'],
       path_style: true
     }
    else
